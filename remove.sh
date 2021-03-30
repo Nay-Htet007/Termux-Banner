98c3a0e2d63c2376e9f91319407978d02b50a52b
Nay Htet <74801775+Nay-Htet007@users.noreply.github.com>
2021-03-30 21:07:10 +0630

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,20 @@
+folder_remove() {
+cd $Home
+if [ -d Termux-Banner ];
+then
+rm -rf Termux-Banner
+fi 
+
+folder_remove
+}
+install_folder() {
+ echo "
+ if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ] 
+command_not_found_handle() {
+/data/data/com.termux/files/usr/libexec/termux/command-not-found
+}
+fi
+PS1='$ ' " >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+install_folder
+}
