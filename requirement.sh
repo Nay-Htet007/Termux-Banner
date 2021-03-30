dcbc68d3529eff4d3ed7cbf3a19fe70a326a932e
Nay Htet <74801775+Nay-Htet007@users.noreply.github.com>
2021-03-30 20:56:44 +0630

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+apt update
+apt upgrade
+apt install figlet -y
+apt install sl -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m Now Runing main process"
+echo
+echo -e "\e[44mCreated by Nay Htet(Cyber-Crack)\e[0m"
+bash banner.sh
