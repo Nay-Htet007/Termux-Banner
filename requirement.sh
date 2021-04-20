18d8ce13c3c9e23156496b011877fb1ea6001026
Nay Htet <74801775+Nay-Htet007@users.noreply.github.com>
2021-04-20 23:14:14 +0630

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -12,7 +12,17 @@ apt install toilet -y
 apt install cowsay -y
 apt install nano -y
 apt install ruby -y
-gem install lolcat -y
+figlet Main
+sleep 1
+figlet Process
+sleep 1
+figlet is
+sleep 1
+figlet Coming
+sleep 1
+figlet Soon
+
+gem install lolcat
 figlet -f big Done !!! | lolcat
 echo
 echo -e "\e[1m Now Runing main process"
